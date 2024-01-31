a0033b7f4e4cbb1a706b449fb34b4ac85046afac
Sebastian Abal Salazar <115430199+cobyzero@users.noreply.github.com>
2024-01-31 18:11:49 -0500

    feat: agregado firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:app_26/Core/Dependencies/injector.dart';
 import 'package:app_26/Core/Routes/router.dart';
+import 'package:app_26/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initDependencies();
   runApp(const MyApp());
 }
